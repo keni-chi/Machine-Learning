95e4e44163807e1b10b22253d00bb5de0d233b23
keni-chi <neko9dog9access@gmail.com>
2024-07-29 09:36:52 +0900

    commit

--- etc/17.eda/eda.py
+++ etc/17.eda/eda.py
@@ -1,59 +1,60 @@
-# EDA
-target = 'target'
+import pandas as pd
+import numpy
+import os
+from ydata_profiling import ProfileReport
+import sweetviz as sv
+from autoviz.AutoViz_Class import AutoViz_Class
 
-#########################
-# # pip install ydata-profiling
-# import pandas as pd
-# from ydata_profiling import ProfileReport
-# data_train = pd.read_csv('./train.csv')
-# profile = ProfileReport(data_train, title="Profiling Report")
-# # profile.to_notebook_iframe()
-# profile.to_file("yp.html")
 
-# #########################
-# pip install sweetviz
-# pip install numpy==1.26.4
-# import sweetviz as sv
-# import pandas as pd
-# import numpy
-# train = pd.read_csv('./train.csv')
-# test = pd.read_csv('./test.csv')
-# my_report = sv.compare([train, "Train"], [test, "Test"], target)
-# # my_report = sv.analyze(train) # trainデータ単体で分析する場合
-# my_report.show_html("sweetviz.html")
+class EDA:
+    def __init__(self):
+        self.target = 'Time'
+        self.df_train = pd.read_csv('./data/input/000.csv', index_col=0)
+        self.df_test = pd.read_csv('./data/input/001.csv', index_col=0).drop([self.target], axis=1)
 
-#########################
-# pip install autoviz
-# pip install IPython
-# pip install jupyter
-# 役立つ：相関係数、カテゴリ毎の目的変数カテゴリでの積み上げ棒グラフ
-# from autoviz.AutoViz_Class import AutoViz_Class
-# import pandas as pd
-# df = pd.read_csv('./train.csv')
-# AV = AutoViz_Class()
-# dft = AV.AutoViz("",                # csvのファイルパスを指定、今回DataFrameデータを既に読み込んだため、空文字""を指定
-#                  depVar=target,   # 列名"target"を目的変数とする。
-#                  dfte=df,    # dfteにはDataFrameデータを指定
-#                  header=0,          # ヘッダー行を指定 
-#                  verbose=1,         # 詳細情報を表示      
-#                  chart_format='html'
-#                  )
-#########################
-# ペアプロットはinteractがよい
-# import numpy as np
-# import pandas as pd
-# from ipywidgets import interact, Select
-# import matplotlib.pyplot as plt
-# %matplotlib inline
+    def make_dir(self):
+        # フォルダ作成
+        dir1 = './data/output/eda'
+        if not os.path.exists(dir1):
+            os.makedirs(dir1)
+        dir1 = './data/output/eda/yp'
+        if not os.path.exists(dir1):
+            os.makedirs(dir1)
+        dir1 = './data/output/eda/sweetviz'
+        if not os.path.exists(dir1):
+            os.makedirs(dir1)
+        dir1 = './data/output/eda/autoviz'
+        if not os.path.exists(dir1):
+            os.makedirs(dir1)
 
-# data = pd.read_csv('./train.csv')
-# def show_plot(col1, col2):
-#     plt.figure(figsize=(6,6))
-#     plt.scatter(data[col1], data[col2], c=data[target])
-#     plt.xlabel('')
-#     plt.ylabel('')
-#     plt.show()
-# w1 = Select(description='X軸:', options=data.columns, rows=4,)
-# w2 = Select(description='Y軸:', options=data.columns, rows=4,)
-# interact(show_plot, col1=w1, col2=w2)
+    def yp(self):
+        # 出力
+        profile = ProfileReport(self.df_train, title='ydata_profiling Report')
+        profile.to_file('./data/output/eda/yp/yp.html')
 
+    def sweetviz(self):
+        my_report = sv.compare([self.df_train, 'Train'], [self.df_test, 'Test'], self.target)
+        my_report.show_html('./data/output/eda/sweetviz/sweetviz.html')
+
+    def autoviz(self):
+        AV = AutoViz_Class()
+        AV.AutoViz(
+            '',
+            depVar=self.target,
+            dfte=self.df_train,
+            header=0,
+            verbose=1, 
+            chart_format='html',
+            save_plot_dir='./data/output/eda/autoviz'
+        )
+
+    def run(self):
+        self.make_dir()
+        self.yp()
+        self.sweetviz()
+        self.autoviz()
+
+
+if __name__ == '__main__':
+    eda = EDA()
+    eda.run()
